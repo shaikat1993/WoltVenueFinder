d128cff6cd4bf823a057a249ac4a686cbb6092b3
Md. Sadidur Rahman <mdsadidurrahman74@gmail.com>
2026-02-13 17:45:07 +0200

    initial commit

---
+++ WoltVenueFinder/WoltVenueFinder/WoltVenueFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoltVenueFinderApp.swift
+//  WoltVenueFinder
+//
+//  Created by Shaikat on 13.2.2026.
+//
+
+import SwiftUI
+
+@main
+struct WoltVenueFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
